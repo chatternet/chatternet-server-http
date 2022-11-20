4050e7983047efb3db6ecc4f32d68bece327b96b
gmcgoldr <garrin.mcgoldrick@gmail.com>
2022-11-20 08:32:14 -0500

    add cli for db and key

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::fs;
 use std::sync::Arc;
 
 use anyhow::Result;
 use chatternet_server_http::chatternet::didkey::{build_jwk, did_from_jwk};
 use clap::Parser;
+use log::info;
 use tokio;
 use tokio::sync::RwLock;
 use warp;
@@ -14,18 +16,30 @@ use chatternet_server_http::handlers::build_api;
 #[command(author, version, about, long_about = None)]
 struct Args {
     port: u16,
-    #[arg(short, long)]
+    path_key: String,
+    path_db: String,
+    #[arg(short = 'k')]
+    new_key: bool,
+    #[arg(short = 'l')]
     loopback: bool,
 }
 
 #[tokio::main]
 async fn main() -> Result<()> {
     pretty_env_logger::init();
-    let jwk = build_jwk(&mut rand::thread_rng())?;
-    let did = did_from_jwk(&jwk)?;
-    println!("DID: {}", &did);
     let args = Args::parse();
-    let connector = Arc::new(RwLock::new(Connector::new("sqlite:db.sqlite").await?));
+
+    if args.new_key {
+        let jwk = build_jwk(&mut rand::thread_rng())?;
+        fs::write(&args.path_key, serde_json::to_string(&jwk)?)?;
+    }
+    let jwk = serde_json::from_str(&fs::read_to_string(&args.path_key)?)?;
+    let did = did_from_jwk(&jwk)?;
+    info!("DID: {}", &did);
+
+    let connector = Arc::new(RwLock::new(
+        Connector::new(&format!("sqlite:{}", args.path_db)).await?,
+    ));
     let routes = build_api(connector, did);
     let address = if args.loopback {
         [127, 0, 0, 1]
@@ -33,5 +47,6 @@ async fn main() -> Result<()> {
         [0, 0, 0, 0]
     };
     warp::serve(routes).run((address, args.port)).await;
+
     Ok(())
 }
